8436366c42fa5d702cc64446d22e0279b1ab5e3f
Parckyzin <jose.silva@cogecom.com.br>
2026-01-16 11:06:14 -0300

    feat(enums): add RepositoryVisibility and CommitType enums

---
+++ src/ai_cli/core/common/enums.py
@@ -0,0 +1,25 @@
+from enum import StrEnum
+
+
+class RepositoryVisibility(StrEnum):
+    """Repository visibility options."""
+
+    PUBLIC = "public"
+    PRIVATE = "private"
+    INTERNAL = "internal"
+
+
+class CommitType(StrEnum):
+    """Conventional commit types."""
+
+    FEAT = "feat"
+    FIX = "fix"
+    DOCS = "docs"
+    STYLE = "style"
+    REFACTOR = "refactor"
+    TEST = "test"
+    CHORE = "chore"
+    BUILD = "build"
+    CI = "ci"
+    PERF = "perf"
+    REVERT = "revert"
