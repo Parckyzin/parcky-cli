96423be66cfd9a6a4a1732710f1e0f63110a95b8
Parckyzin <jose.silva@cogecom.com.br>
2026-01-21 15:23:18 -0300

    feat(models): add CommitResult and SmartCommitAllResult

--- src/ai_cli/core/models/__init__.py
+++ src/ai_cli/core/models/__init__.py
@@ -1,16 +1,22 @@
+from .commit_message import CommitMessage
 from .git_diff import GitDiff
 from .git_branch import GitBranch
 from .file_change import FileChange
-from .file_group import FileGroup
+from .file_group import CommitResult, FileGroup, SmartCommitAllResult
 from .repository import Repository
 from .pull_request import PullRequest
+from ai_cli.core.common.enums import CommitType
 
 
 __all__ = [
+    "CommitMessage",
+    "CommitType",
     "GitDiff",
     "GitBranch",
     "FileChange",
     "FileGroup",
+    "CommitResult",
+    "SmartCommitAllResult",
     "Repository",
     "PullRequest",
 ]
