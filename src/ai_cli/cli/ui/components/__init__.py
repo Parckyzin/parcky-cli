f6d75ecd0c982f09c6a05885c0ef23b46f7f4b51
Parckyzin <jose.silva@cogecom.com.br>
2026-01-28 11:01:57 -0300

    refactor(ui): move table rendering to separate module

--- src/ai_cli/cli/ui/components/__init__.py
+++ src/ai_cli/cli/ui/components/__init__.py
@@ -1,13 +1,6 @@
-from .select import (
-    SelectOption,
-    SelectResult,
-    SelectState,
-    TableColumnSpec,
-    handle_key,
-    render_table,
-    select,
-)
+from .select import SelectOption, SelectResult, SelectState, handle_key, select
 from .theme import DEFAULT_THEME, Theme
+from ..renderers.select_table import TableColumnSpec, render_table
 
 __all__ = [
     "DEFAULT_THEME",
